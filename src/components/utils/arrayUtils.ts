244abbf1918d475793c388d97aeb081aced49feb
bishal_giri <bishal_giri@bishal-giris-MacBook-Air.local>
2022-02-20 00:15:24 +0545

    added some useful helper array methods

--- src/components/utils/arrayUtils.ts
+++ src/components/utils/arrayUtils.ts
@@ -2,9 +2,25 @@ type Item = {
   id: string;
 };
 
-export const findItemIndexById = <TItem extends Item>(
-  items: TItem[],
-  id: string
-) => {
-  return items.findIndex((item: TItem) => item.id === id);
+// find index of the item in the array and return it
+export const findItemIndexById = (items: Item[], id: string) => {
+  return items.findIndex((item: Item) => item.id === id);
 };
+
+// ----------- moving item from the array
+export const moveItem = (array: Item[], from: number, to: number) => {
+  const item = array[from];
+  return insertItemAtIndex(removeItemsAtIndex(array, from), item, to);
+};
+
+// -------------- PRIVATE FUNCTIONS
+
+// ------------ removing items at index
+export function removeItemsAtIndex(array: Item[], index: number) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
+
+// ----------- inserting items at index
+export function insertItemAtIndex(array: Item[], item: Item, index: number) {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+}
